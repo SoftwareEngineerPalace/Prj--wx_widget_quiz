07ba0161f46a096dd72cd2906445f04eebc7c913
xiaojianjun <jianjun.xiao@foxmail.com>
2023-09-25 20:59:01 +0800

    loading

--- pages/task/index.vue
+++ pages/task/index.vue
@@ -1,92 +1,96 @@
 <template>
-	<view class="base">
+	<view>
+		<view class="base">
+			<!-- 上部按钮 -->
+			<view class="topBar">
+				<u-textarea autoHeight class="initime__input" v-model="initTimeRaw" placeholder="请输入起始时间" />
+				<button type="default" class="initime__ok" @click="onConfirmInitTime">确定</button>
+				<button type="primary" class="btn-now" @click="setNowForStart">设定当前为起始时间</button>
+				<button type="warn" class="btn-add" @click="addOne">新增</button>
+			</view>
 
-		<!-- 上部按钮 -->
-		<view class="topBar">
-			<u-textarea autoHeight class="initime__input" v-model="initTimeRaw" placeholder="请输入起始时间" />
-			<button type="default" class="initime__ok" @click="onConfirmInitTime">确定</button>
-			<button type="primary" class="btn-now" @click="setNowForStart">设定当前为起始时间</button>
-			<button type="warn" class="btn-add" @click="addOne">新增</button>
-		</view>
+			<!-- 拖拽容器 -->
+			<view class="bottom-container">
+				<view class="wrapper" ref="listRef">
+					<view class="container" v-for="(item, index) in list" :key="item.id">
 
-		<!-- 拖拽容器 -->
-		<view class="bottom-container">
-			<view class="wrapper" ref="listRef">
-				<view class="container" v-for="(item, index) in list" :key="item.id">
+						<!-- 截止时间 -->
+						<view class="deadline" :style="{color: colorDic[item.priority]}">{{ `${item.deadline}` }}</view>
 
-					<!-- 截止时间 -->
-					<view class="deadline" :style="{color: colorDic[item.priority]}">{{ `${item.deadline}` }}</view>
+						<!-- 任务名字 -->
+						<u-textarea :customStyle="{marginLeft:'8px', width:'100px'}" class="name" autoHeight
+							v-model="item.name" placeholder="任务" @blur="onBlur">
+						</u-textarea>
 
-					<!-- 任务名字 -->
-					<u-textarea :customStyle="{marginLeft:'8px', width:'100px'}" class="name" autoHeight
-						v-model="item.name" placeholder="任务" @blur="onBlur">
-					</u-textarea>
+						<!-- 选择优先级 -->
+						<view :style="{marginLeft:'8px'}">
+							<u-radio-group :customStyle="{marginLeft:'8px'}" size='10px' v-model="item.priority"
+								@change="priorityChanged" class="priority-group" placement="column">
+								<u-radio iconSize="12" labelSize="12px" :key="3" :label="3" :name="3" shape="square"
+									:customStyle="{marginRight: '8px'}">高</u-radio>
+								<u-radio labelSize="12px" :key="2" :label="2" :name="2" shape="square"
+									:customStyle="{marginRight: '8px'}">中</u-radio>
+								<u-radio labelSize="12px" :key=" 1" :label="1" :name="1" shape="square"
+									:customStyle="{marginRight: '8px'}">低</u-radio>
+							</u-radio-group>
+						</view>
 
-					<!-- 选择优先级 -->
-					<view :style="{marginLeft:'8px'}">
-						<u-radio-group :customStyle="{marginLeft:'8px'}" size='10px' v-model="item.priority"
-							@change="priorityChanged" class="priority-group" placement="column">
-							<u-radio iconSize="12" labelSize="12px" :key="3" :label="3" :name="3" shape="square"
-								:customStyle="{marginRight: '8px'}">高</u-radio>
-							<u-radio labelSize="12px" :key="2" :label="2" :name="2" shape="square"
-								:customStyle="{marginRight: '8px'}">中</u-radio>
-							<u-radio labelSize="12px" :key=" 1" :label="1" :name="1" shape="square"
-								:customStyle="{marginRight: '8px'}">低</u-radio>
-						</u-radio-group>
+						<!-- 选择工作时长 -->
+						<view class="container-duraion">
+							<u-radio-group size='10px'
+								:customStyle="{ backgroundColor:'blue', display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)'}"
+								v-model="item.duration" @change="onDurationChange" class="duration-group">
+								<u-grid :col="3">
+									<u-grid-item>
+										<u-radio labelSize="12px" :key="10" :name="10" :label="10" shape="square"
+											:customStyle="{marginRight: '4px'}">10</u-radio>
+									</u-grid-item>
+									<u-grid-item>
+										<u-radio labelSize="12px" :key="20" :name="20" :label="20" shape="square"
+											:customStyle="{marginRight: '4px'}">20</u-radio>
+									</u-grid-item>
+									<u-grid-item>
+										<u-radio labelSize="12px" :key="30" :name="30" :label="30" shape="square"
+											:customStyle="{marginRight: '4px'}">30</u-radio>
+									</u-grid-item>
+									<u-grid-item>
+										<u-radio labelSize="12px" :key="40" :name="40" :label="40" shape="square"
+											:customStyle="{marginRight: '4px'}">40</u-radio>
+									</u-grid-item>
+									<u-grid-item>
+										<u-radio labelSize="12px" :key="60" :name="60" :label="60" shape="square"
+											:customStyle="{marginRight: '4px'}">60</u-radio>
+									</u-grid-item>
+									<u-grid-item>
+										<u-radio labelSize="12px" :key="90" :name="90" :label="90" shape="square"
+											:customStyle="{marginRight: '4px'}">90</u-radio>
+									</u-grid-item>
+								</u-grid>
+							</u-radio-group>
+						</view>
+
+						<!-- 删除按钮 -->
+						<view class="container-action">
+							<button class="btn delete" @click="onDelete(index)">删</button>
+							<button class="btn up" @click="moveUp(index)" v-if="index !== 0">上</button>
+							<button class="btn down" @click="moveDown(index)"
+								v-if="index !== list.length - 1">下</button>
+						</view>
+
 					</view>
-
-					<!-- 选择工作时长 -->
-					<view class="container-duraion">
-						<u-radio-group size='10px'
-							:customStyle="{ backgroundColor:'blue', display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)'}"
-							v-model="item.duration" @change="onDurationChange" class="duration-group">
-							<u-grid :col="3">
-								<u-grid-item>
-									<u-radio labelSize="12px" :key="10" :name="10" :label="10" shape="square"
-										:customStyle="{marginRight: '4px'}">10</u-radio>
-								</u-grid-item>
-								<u-grid-item>
-									<u-radio labelSize="12px" :key="20" :name="20" :label="20" shape="square"
-										:customStyle="{marginRight: '4px'}">20</u-radio>
-								</u-grid-item>
-								<u-grid-item>
-									<u-radio labelSize="12px" :key="30" :name="30" :label="30" shape="square"
-										:customStyle="{marginRight: '4px'}">30</u-radio>
-								</u-grid-item>
-								<u-grid-item>
-									<u-radio labelSize="12px" :key="40" :name="40" :label="40" shape="square"
-										:customStyle="{marginRight: '4px'}">10</u-radio>
-								</u-grid-item>
-								<u-grid-item>
-									<u-radio labelSize="12px" :key="60" :name="60" :label="60" shape="square"
-										:customStyle="{marginRight: '4px'}">20</u-radio>
-								</u-grid-item>
-								<u-grid-item>
-									<u-radio labelSize="12px" :key="90" :name="90" :label="90" shape="square"
-										:customStyle="{marginRight: '4px'}">30</u-radio>
-								</u-grid-item>
-							</u-grid>
-						</u-radio-group>
-					</view>
-
-					<!-- 删除按钮 -->
-					<view class="container-action">
-						<button class="btn delete" @click="onDelete(index)">删</button>
-						<button class="btn up" @click="moveUp(index)" v-if="index !== 0">上</button>
-						<button class="btn down" @click="moveDown(index)" v-if="index !== list.length - 1">下</button>
-					</view>
-
 				</view>
 			</view>
+
+			<u-toast ref="uToast"></u-toast>
 		</view>
+		<u-loading-page color="#4cd964" loading-color="#4cd964" icon-size="40" font-size="40" class="loading" bg-color="#bbbbbb00"
+			loading-text="正在执行" :loading="showLoading"></u-loading-page>
 	</view>
 </template>
 
 <script lang="ts">
-	declare const wx : any;
-
 	import dayjs from "dayjs";
-	import { formatTime, generateUUID } from '../../common/utils';
+	import { formatTime, generateUUID, showToast } from '../../common/utils';
 
 	interface ITask {
 		id : string;
@@ -112,7 +116,8 @@
 				listRef: null,
 				initTime: 8 * 60 + 30,
 				initTimeRaw: "8:30",
-				list: []
+				list: [],
+				showLoading: false,
 			}
 		},
 		mounted() {
@@ -144,6 +149,7 @@
 				}
 			},
 			addOne() {
+				this.showLoading = true;
 				const one = {
 					name: "任务",
 					duration: 10,
@@ -157,6 +163,7 @@
 			},
 			/** 确认了初始时间 */
 			onConfirmInitTime() {
+				this.showLoading = true;
 				const a = this.initTimeRaw.split(":");
 				const b = this.initTimeRaw.split("：");
 				const times = a.length === 2 ? a : b;
@@ -169,6 +176,7 @@
 
 			/** 设置当前为开始 */
 			setNowForStart() {
+				this.showLoading = true;
 				const now = dayjs(); // 获取当前时间
 				let hour = now.hour(); // 获取当前时间的小时部分
 				const minute = now.minute(); // 获取当前时间的分钟部分
@@ -192,24 +200,28 @@
 
 			/** 优先级更新了 */
 			priorityChanged() {
+				this.showLoading = true;
 				console.log("priorityChanged");
 				this.update();
 			},
 
 			/** 时长更新了 */
 			onDurationChange() {
+				this.showLoading = true;
 				console.log("onDurationChange");
 				this.update();
 			},
 
 			/** 被删除了 */
 			onDelete(index : number) {
+				this.showLoading = true;
 				this.list = this.list.slice(0, index).concat(this.list.slice(index + 1));
 				console.log("onDelete");
 				this.update();
 			},
 			moveUp(index : number) {
 				if (index === 0) return;
+				this.showLoading = true;
 				const temp = this.list[index];
 				this.list[index] = this.list[index - 1];
 				this.list[index - 1] = temp;
@@ -217,6 +229,7 @@
 			},
 			moveDown(index : number) {
 				if (index === this.list.length - 1) return;
+				this.showLoading = true;
 				const temp = this.list[index];
 				this.list[index] = this.list[index + 1];
 				this.list[index + 1] = temp;
@@ -251,6 +264,11 @@
 					data: { list: _list }
 				})
 				console.log('保存数据后的回调', rsp);
+				const result = rsp.errMsg === "cloud.callFunction:ok";
+				if (result) {
+					showToast(this, "操作成功");
+					this.showLoading = false;
+				}
 			},
 			onBlur() {
 				console.log('移动到最上面');
@@ -260,6 +278,14 @@
 </script>
 
 <style scoped lang="scss">
+	.loading {
+		z-index: 1000;
+	}
+
+	.vue-ref {
+		z-index: 1000;
+	}
+
 	.base {
 		width: 100vw;
 		color: black;
