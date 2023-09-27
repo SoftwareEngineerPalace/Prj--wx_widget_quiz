0dc5451a1e296c5ce795d358c3147a1824d45d8c
xiaojianjun <jianjun.xiao@foxmail.com>
2023-09-27 15:33:26 +0800

    mine 页转 vue3

--- pages/mine/index.vue
+++ pages/mine/index.vue
@@ -88,104 +88,101 @@
 	</view>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 	import { checkSession } from '../../common/utils';
-	import queryString from 'query-string'
+	import queryString from 'query-string';
 
-	export default {
-		data() {
-			return {
-				loggedIn: false,
-				codeRef: "",
-				loginInfo: { nickName: "我的昵称", avatarUrl: '' },
-				adminVisible: false
-			}
-		},
-		async onMounted() {
-			wx.cloud.init({
-				env: "quiz-0gb2aw2vb2850af4"
-			});
+	import { ref, onMounted } from 'vue';
+	const loggedIn = ref(false);
+	const codeRef = ref("");
+	const loginInfo = ref({ nickName: "我的昵称", avatarUrl: '' });
+	const adminVisible = ref(false);
 
-			const hasSession = await checkSession();
-			const token = uni.getStorageSync('token');
-			this.loggedIn = hasSession as boolean && !!token;
-			console.log('是已登录状态', this.loggedIn);
-		},
-		methods: {
-			onCms(evt : any) {
-				// console.log('onCms evt', evt);
-				const { quiztype } = evt.target.dataset;
-				const queryStr = queryString.stringify({ quizType: quiztype })
-				const url = `/pages/cms/index?${queryStr}`;
-				// console.log('onCms', url);
-				uni.navigateTo({ url });
-			},
-			onTask() {
-				const url = `/pages/task/index`;
-				uni.navigateTo({ url });
-			},
-			async getUserInfo() {
-				uni.showLoading({
-					title: '加载中',
-				});
-				uni.getUserProfile({
-					desc: '登录后可同步数据',
-					success: async (value) => {
-						const { nickName, avatarUrl } = value.userInfo;
-						this.loginInfo = { nickName, avatarUrl };
-						console.log('loginInfo', this.loginInfo)
-						uni.login({
-							provider: 'weixin',
-							success: async (rsp) => {
-								console.log('uni.login rsp', rsp);
-								const { code } = rsp;
-								this.codeRef = code;
-								// 请求登录接口
-								if (rsp.errMsg == 'login:ok') {
-									const data = { code, nickName, avatarUrl };
-									console.log("applet call login", data)
-									const rsp : any = await wx.cloud.callFunction({
-										name: 'login',
-										data
-									});
-									console.log("applet call login callback", rsp)
-									if (rsp.result.status === 200) {
-										console.log('uni.setStorageSync', rsp.result.token);
-										uni.setStorageSync('token', rsp.result.token)
-										this.loggedIn = true;
-									}
-								}
-							},
-						});
-					},
-					fail: () => {
-						uni.showToast({
-							title: '授权已取消',
-							icon: 'error',
-							mask: true,
-						});
-					},
-					complete: () => {
-						// 隐藏loading
-						uni.hideLoading();
-					},
-				});
-			},
-			logout() {
-				uni.showModal({
-					title: '确定要退出登录吗？',
-					success: function (res) {
-						if (res.confirm) {
-							uni.removeStorageSync('token')
-							this.loginInfo = { nickname: "", avatarUrl: '' };
-							this.loggedIn = false;
-						} else if (res.cancel) {
-							console.log('用户点击取消');
+	onMounted(async () => {
+		wx.cloud.init({
+			env: "quiz-0gb2aw2vb2850af4"
+		});
+
+		const hasSession = await checkSession();
+		const token = uni.getStorageSync('token');
+		loggedIn.value = hasSession as boolean && !!token;
+		console.log('是已登录状态', loggedIn);
+	})
+
+	const onCms = (evt : any) => {
+		// console.log('onCms evt', evt);
+		const { quiztype } = evt.target.dataset;
+		const queryStr = queryString.stringify({ quizType: quiztype })
+		const url = `/pages/cms/index?${queryStr}`;
+		// console.log('onCms', url);
+		uni.navigateTo({ url });
+	};
+
+	const onTask = () => {
+		const url = `/pages/task/index`;
+		uni.navigateTo({ url });
+	};
+
+	const getUserInfo = async () => {
+		uni.showLoading({
+			title: '加载中',
+		});
+		uni.getUserProfile({
+			desc: '登录后可同步数据',
+			success: async (value) => {
+				const { nickName, avatarUrl } = value.userInfo;
+				loginInfo.value = { nickName, avatarUrl };
+				console.log('loginInfo', loginInfo)
+				uni.login({
+					provider: 'weixin',
+					success: async (rsp) => {
+						console.log('uni.login rsp', rsp);
+						const { code } = rsp;
+						codeRef.value = code;
+						// 请求登录接口
+						if (rsp.errMsg == 'login:ok') {
+							const data = { code, nickName, avatarUrl };
+							console.log("applet call login", data)
+							const rsp : any = await wx.cloud.callFunction({
+								name: 'login',
+								data
+							});
+							console.log("applet call login callback", rsp)
+							if (rsp.result.status === 200) {
+								console.log('uni.setStorageSync', rsp.result.token);
+								uni.setStorageSync('token', rsp.result.token)
+								loggedIn.value = true;
+							}
 						}
-					}
+					},
 				});
+			},
+			fail: () => {
+				uni.showToast({
+					title: '授权已取消',
+					icon: 'error',
+					mask: true,
+				});
+			},
+			complete: () => {
+				// 隐藏loading
+				uni.hideLoading();
+			},
+		});
+	};
+	const logout = () => {
+		uni.showModal({
+			title: '确定要退出登录吗？',
+			success: function (res) {
+				if (res.confirm) {
+					uni.removeStorageSync('token')
+					loginInfo.value = { nickName: "", avatarUrl: '' };
+					loggedIn.value = false;
+				} else if (res.cancel) {
+					console.log('用户点击取消');
+				}
 			}
-		}
+		});
 	}
 </script>
 
